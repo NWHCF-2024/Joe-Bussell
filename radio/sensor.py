5a58c8b59d152de64604f83a927a08870113c5ab
Joe-Engineer <joe_bussell@hotmail.com>
2024-09-22 22:32:57 -0700

    Update sensor.py

--- radio/sensor.py
+++ radio/sensor.py
@@ -1,37 +1,33 @@
-# Transmitter Code
+# Sensor Code
 from microbit import *
 import radio
+import random
 
-# Initialize the radio
+def generate_unique_id():
+    seed = running_time() + temperature() + accelerometer.get_x()
+    random.seed(seed)
+    uid = ''.join([chr(random.randint(65, 90)) for _ in range(6)])
+    return uid
+
+unique_id = generate_unique_id()
+
 radio.on()
-radio.config(channel=7)  # Ensure both micro:bits are on the same channel
-
-transmitting = False  # Control variable for transmission
+radio.config(channel=7)  # Use a common channel
 
 while True:
-    # Listen for start/stop commands from the receiver
-    incoming = radio.receive()
-    if incoming == 'start':
-        transmitting = True
-    elif incoming == 'stop':
-        transmitting = False
-
-    if transmitting:
-        # Measure temperature
-        temp = temperature()
-        
-        # Measure voltage on pin0
-        scale = 3.3 / 1024  # Micro:bit uses 3.3V logic levels
-        voltage = pin0.read_analog() * scale
-        voltage = round(voltage, 2)
-        
-        # Prepare data string
-        data = "{},{}".format(temp, voltage)
-        
-        # Send data via radio
-        radio.send(data)
-        
-        # Small delay between transmissions
-        sleep(1000)  # Transmit every 1 second
-    else:
-        sleep(100)
+    # Measure temperature
+    temp = temperature()
+    
+    # Measure voltage
+    scale = 3.3 / 1024
+    voltage = pin0.read_analog() * scale
+    voltage = round(voltage, 2)
+    
+    # Prepare data
+    data = "{},{},{}".format(unique_id, temp, voltage)
+    
+    # Send data
+    radio.send(data)
+    
+    # Random delay to reduce collisions
+    sleep(random.randint(900, 1100))
